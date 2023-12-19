23f40b5f46ab0f23594614e72d1f0069e9f0b849
adwitkow <adamadik@poczta.fm>
2023-12-19 13:39:11 +0100

    Add project files.

---
+++ LeetCode.Tests/1913_Tests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+    internal class _1913_Tests
+    {
+        [Test]
+    }
+}
---
+++ LeetCode/1913.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode
+{
+    // https://leetcode.com/problems/maximum-product-difference-between-two-pairs/
+    internal class _1913
+    {
+        public int MaxProductDifference(int[] nums)
+        {
+            Array.Sort(nums);
+
+            // not the prettiest or most performant but I'm a huge fan of LINQ :)
+            var lowerProduct = nums.Take(2).Aggregate(1, (acc, val) => acc * val);
+            var higherProduct = nums.TakeLast(2).Aggregate(1, (acc, val) => acc * val);
+
+            return higherProduct - lowerProduct;
+        }
+
+        [Test]
+        [TestCase(new int[] { 5, 6, 2, 7, 4 }, 34)]
+        [TestCase(new int[] { 4, 2, 5, 9, 7, 4, 8 }, 64)]
+        public void Test(int[] numbers, int expected)
+        {
+            var result = MaxProductDifference(numbers);
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
