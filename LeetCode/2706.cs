e5f5bed564b5f231b3089812f5e76d32b265c37e
adwitkow <adamadik@poczta.fm>
2023-12-20 15:59:27 +0100

    2706. Buy Two Chocolates

---
+++ LeetCode/2706.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json;
+using NUnit.Framework;
+
+namespace LeetCode
+{
+    internal class _2706
+    {
+        // https://leetcode.com/problems/buy-two-chocolates/
+        public int BuyChoco(int[] prices, int money)
+        {
+            Array.Sort(prices);
+
+            var currentMoney = money;
+            for (int i = 0; i < 2; i++)
+            {
+                currentMoney -= prices[i];
+            }
+
+            return currentMoney < 0 ? money : currentMoney;
+        }
+
+        [Test]
+        [TestCase("[1,2,2]", 3, 0)]
+        [TestCase("[3,2,3]", 3, 3)]
+        public void Test(string input, int money, int expected)
+        {
+            var numbers = JsonSerializer.Deserialize<int[]>(input);
+            var result = BuyChoco(numbers, money);
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
