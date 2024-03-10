c413c60da257f00b59bbab8527125d41ef23cfcc
adwitkow <adamadik@poczta.fm>
2024-03-10 21:11:25 +0100

    38. Count and Say

---
+++ LeetCode/30-39/38.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+using NUnit.Framework;
+
+namespace LeetCode._30_39
+{
+    // https://leetcode.com/problems/count-and-say/
+    public class _38
+    {
+        public string CountAndSay(int n)
+        {
+            if (n <= 1)
+            {
+                return "1";
+            }
+
+            var builder = new StringBuilder();
+
+            var previousResult = CountAndSay(n - 1);
+            var previousChar = previousResult[0];
+            var count = 0;
+
+            for (int i = 0; i < previousResult.Length; i++)
+            {
+                var ch = previousResult[i];
+                if (ch == previousChar)
+                {
+                    count++;
+                }
+                else
+                {
+                    builder.Append(count);
+                    builder.Append(previousChar);
+
+                    previousChar = ch;
+                    count = 1;
+                }
+            }
+
+            builder.Append(count);
+            builder.Append(previousChar);
+
+            return builder.ToString();
+        }
+
+        [Test]
+        [TestCase(1, "1")]
+        [TestCase(4, "1211")]
+        [TestCase(5, "111221")]
+        public void Test(int input, string expected)
+        {
+            var result = CountAndSay(input);
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
